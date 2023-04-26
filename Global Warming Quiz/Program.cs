b3b4d011838ef98a78147912e899a18bdaf4f236
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-26 10:30:40 +0500

    minor changes in the code

--- Global Warming Quiz/Program.cs
+++ Global Warming Quiz/Program.cs
@@ -11,7 +11,10 @@
         Console.WriteLine("   2. Natural cycles");
         Console.WriteLine("   3. Solar radiation");
         Console.WriteLine("   4. Volcanic eruptions");
+        Console.Write("\nEnter You Answer (1, 2, 3 or 4): ");
         int answerOne = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing purpose
 
         // Question 2
         Console.WriteLine("2. What is the greenhouse effect?");
@@ -19,7 +22,10 @@
         Console.WriteLine("   2. The cooling of Earth's atmosphere by water vapor");
         Console.WriteLine("   3. The depletion of Earth's ozone layer");
         Console.WriteLine("   4. The warming of Earth's atmosphere by cosmic rays");
+        Console.Write("\nEnter You Answer (1, 2, 3 or 4): ");
         int answerTwo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing purpose
 
         // Question 3
         Console.WriteLine("3. How much has Earth's temperature increased since the Industrial Revolution?");
@@ -27,7 +33,10 @@
         Console.WriteLine("   2. 5 degrees Fahrenheit");
         Console.WriteLine("   3. 10 degrees Fahrenheit");
         Console.WriteLine("   4. 15 degrees Fahrenheit");
+        Console.Write("\nEnter You Answer (1, 2, 3 or 4): ");
         int answerThree = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing purpose
 
         // Question 4
         Console.WriteLine("4. How do greenhouse gases contribute to global warming?");
@@ -35,7 +44,10 @@
         Console.WriteLine("   2. By absorbing heat in the atmosphere");
         Console.WriteLine("   3. By blocking heat from reaching Earth's surface");
         Console.WriteLine("   4. By causing volcanic eruptions");
+        Console.Write("\nEnter You Answer (1, 2, 3 or 4): ");
         int answerFour = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing purpose
 
         // Question 5
         Console.WriteLine("5. What are some of the consequences of global warming?");
@@ -43,6 +55,9 @@
         Console.WriteLine("   2. Rising sea levels");
         Console.WriteLine("   3. More frequent and severe weather events");
         Console.WriteLine("   4. All of the above");
+        Console.Write("\nEnter You Answer (1, 2, 3 or 4): ");
         int answerFive = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine();    // just for spacing purpose
     }
 }
